8080ea48fc269aa06b66bb2dd5eb6be486b378ac
ardarawk-cloud <ardarawk@gmail.com>
2026-08-12 15:51:14 +0800

    fix: route ENTEGO production auth to clean store v2

---
+++ api-worker-v50.js
@@ -0,0 +1,4 @@
+import core from './api-worker-v43.js';
+export {EntegoStore,EntegoPayment,EntegoPartner,EntegoChat,EntegoOps} from './api-worker-v43.js';
+export {EntegoAuth} from './auth-store-v2.js';
+export default core;
